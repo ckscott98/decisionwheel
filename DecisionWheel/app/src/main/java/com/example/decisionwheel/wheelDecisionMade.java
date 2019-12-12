a2e8fcf02f556c62464f31f8a68ba6d0b784ce29
Cheyenne <ckscott@umd.edu>
2019-12-11 22:35:51 -0500

    Cheyenne wheel done

--- DecisionWheel/app/src/main/java/com/example/decisionwheel/wheelDecisionMade.java
+++ DecisionWheel/app/src/main/java/com/example/decisionwheel/wheelDecisionMade.java
@@ -2,7 +2,10 @@ package com.example.decisionwheel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class wheelDecisionMade extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class wheelDecisionMade extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wheel_decision_made);
+
+        Intent i = getIntent();
+        String decision = i.getStringExtra("wheelDecision");
+
+        TextView txt = findViewById(R.id.wheelResultText);
+        txt.setText(decision);
+    }
+
+    public void goHome(View view) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
     }
 }
